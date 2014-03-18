128b39128f282d935124f44e1beeebc806ccc91c
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2014-03-18 20:38:59 +0000

    Fix quote style.

--- lib/jekyll/invoice/filters.rb
+++ lib/jekyll/invoice/filters.rb
@@ -8,11 +8,11 @@ module Jekyll
       def effective(date_ranges, field_list)
         effective_date = self.effective_date
         match = date_ranges.detect do |date_range|
-          end_date = date_range["end"]
+          end_date = date_range['end']
           range = if end_date.nil?
-                    date_range["start"].to_time...Time.now
+                    date_range['start'].to_time...Time.now
                   else
-                    date_range["start"].to_time...end_date.to_time
+                    date_range['start'].to_time...end_date.to_time
                   end
           range.cover?(effective_date)
         end
