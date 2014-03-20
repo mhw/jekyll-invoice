b51ad5b7e3cffa8ec79a32057281444a07831f21
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2014-03-20 11:10:26 +0000

    jekyll pulls in safe_yaml, which breaks guard-minitest. Workaround.
    guard-minitest runs the tests in a subprocess and passes the notifier
    configuration as a YAML dump in the GUARD_NOTIFIERS environment
    variable. This fails if safe_yaml is being used in the child process
    as the symbols will be converted to strings. Our workaround is to
    load safe_yaml as early as possible through the test_helper.rb file
    and enable symbol parsing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
+# Work around breaking change in safe_yaml
+require 'safe_yaml'
+YAML.enable_symbol_parsing!
 
 require 'jekyll-invoice'
 require 'minitest/autorun'
-require 'yaml'
 
 def load_data(file)
   YAML.load_file(File.join('templates/uk/_data', file))
